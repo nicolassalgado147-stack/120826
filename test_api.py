85c198f5e6bc0eb5922b319dc7a0fdeebdb78d32
nicolassalgado147 <nicolassalgado147@gmail.com>
2026-08-26 23:13:44 -0500

    fix: agregar tiempo de espera sleep para inicializar API

--- test_api.py
+++ test_api.py
@@ -1,12 +1,6 @@
 import requests
 
 def test_get_root_200_ok():
-    # Petición a la API local ANTES del despliegue
-    # Si tu contenedor en docker-compose expone otro puerto (ej. 5058), cámbialo aquí
     url = "http://localhost:5050/"
-    
-    try:
-        response = requests.get(url)
-        assert response.status_code == 200, f"Se esperaba 200, pero la API respondió con {response.status_code}"
-    except requests.exceptions.ConnectionError:
-        assert False, "No se pudo establecer conexión con la API en local."
+    response = requests.get(url)
+    assert response.status_code == 200, f"Se esperaba 200 OK pero se obtuvo {response.status_code}"
